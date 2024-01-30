4b23d2e8ffcb3c2ddd77e99f7e944136058f69e0
okste1234 <okste1234@gmail.com>
2024-01-30 16:05:20 +0100

    laying navbar for mobile

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,42 @@
+/* eslint-disable react/jsx-indent-props */
+/* eslint-disable no-unused-vars */
 import React from 'react';
+import {
+    AppBar,
+    IconButton,
+    Toolbar,
+    Drawer,
+    Button,
+    Avatar,
+    useMediaQuery,
+} from '@mui/material';
+import {
+    Menu,
+    AccountCircle,
+    Brightness4,
+    Brightness7,
+} from '@mui/icons-material';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
+    const isMobile = useMediaQuery('(max-width : 600px)');
+
     return (
-        <div>NavBar</div>
+        <AppBar position="fixed">
+            <Toolbar>
+                {isMobile && (
+                    <IconButton
+                        color="inherit"
+                        edge="start"
+                        style={{ outline: 'none' }}
+                        onClick={() => { }}
+                        className="menuButton"
+                    >
+                        <Menu />
+                    </IconButton>
+                )}
+            </Toolbar>
+        </AppBar>
     );
 }
 
